9a2d4bdb6570401f0a7b3d1b5e5a8a2275aa26d4
cheng yang <1@222.com>
2021-08-11 21:36:16 +0800

    独角数卡usdt支付接口
    独角数卡usdt支付接口

---
+++ 1.x/app/Http/Controllers/Pay/Token188Controller.php
@@ -0,0 +1,146 @@
+<?php
+namespace App\Http\Controllers\Pay;
+
+use App\Exceptions\AppException;
+use App\Models\Pays;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redis;
+use Yansongda\Pay\Pay;
+
+class Token188Controller extends PayController
+{
+    public function gateway($payway, $oid)
+    {
+        $this->checkOrder($payway, $oid);
+		$params = [
+            'merchantId' => $this->payInfo['merchant_id'],
+            'outTradeNo' => $this->orderInfo['order_id'],
+            'subject' => $this->orderInfo['order_id'],
+            'totalAmount' => $this->orderInfo['actual_price'],
+            'attach' => $this->orderInfo['actual_price'],
+            'body' => $this->orderInfo['order_id'],
+            'coinName' => 'USDT-TRC20',
+            'notifyUrl' => site_url() . $this->payInfo['pay_handleroute'] . '/notify_url',
+            'timestamp' => $this->msectime(),
+            'nonceStr' => $this->getNonceStr(16)
+        ];
+        
+        //echo $params['totalAmount'];
+        $mysign = self::GetSign($this->payInfo['merchant_pem'], $params);
+        // 网关连接
+        $ret_raw = self::_curlPost('https://api.token188.com/utg/pay/address', $params,$mysign,1);
+		$ret = @json_decode($ret_raw, true);
+		if($ret['rst']=='300'){
+		    echo "获取参数失败，请重新提交";
+		}else{
+		    header("Location: ".$ret['data']['paymentUrl']);
+		}
+		
+		
+        exit;
+    }
+
+    public function notifyUrl(Request $request)
+    {
+        $content = file_get_contents('php://input');
+		$json_param = json_decode($content, true); //convert JSON into array
+        
+        $cacheord = json_decode(Redis::hget('PENDING_ORDERS_LIST', $json_param['outTradeNo']), true);
+        if (!$cacheord) {
+            return 'fail';
+        }
+        
+        $payInfo = Pays::where('id', $cacheord['pay_way'])->first();
+		
+		$coinPay_sign = $json_param['sign'];
+		unset($json_param['sign']);
+		unset($json_param['notifyId']);
+		$sign = self::GetSign($payInfo['merchant_pem'], $json_param);
+		if ($sign !== $coinPay_sign) {
+			echo json_encode(['status' => 400]);
+			return false;
+		}
+		$json_param['sign'] = $sign;
+
+		// check request format
+		if ($json_param['merchantId']!=$payInfo['merchant_id']) {
+			echo json_encode(['status' => 401]);
+			return false;
+		}
+
+           
+        $out_trade_no = $json_param['outTradeNo'];
+		// check payment status
+		if ($json_param['tradeStatus'] === 'SUCCESS') {
+			$pay_trade_no=$json_param['tradeNo'];
+			$price=($json_param['originalAmount']);
+			
+            $this->orderService->successOrder($out_trade_no, $pay_trade_no, $price);
+			
+
+			echo 'success';
+			die();
+			return true;
+		} 
+
+		echo json_encode(['status' => 406]);
+		return false;
+
+    }
+	private function _curlPost($url,$params=false,$signature,$ispost=0){
+        
+		$ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 300); //设置超时
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); // https请求 不验证证书和hosts
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+        curl_setopt(
+            $ch, CURLOPT_HTTPHEADER, array('token:'.$signature)
+        );
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
+	public function GetSign($secret, $params)
+    {
+        $p=ksort($params);
+        reset($params);
+
+		if ($p) {
+			$str = '';
+			foreach ($params as $k => $val) {
+				$str .= $k . '=' .  $val . '&';
+			}
+			$strs = rtrim($str, '&');
+		}
+		$strs .='&key='.$secret;
+        
+        $signature = md5($strs);
+
+        //$params['sign'] = base64_encode($signature);
+        return $signature;
+    }
+    public function msectime() {
+		list($msec, $sec) = explode(' ', microtime());
+		$msectime = (float)sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);
+		return $msectime;
+    }
+    /**
+     * 返回随机字符串
+     * @param int $length
+     * @return string
+     */
+    public static function getNonceStr($length = 32)
+    {
+        $chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+        $str = "";
+        for ($i = 0; $i < $length; $i++) {
+            $str .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
+        }
+        return $str;
+    }
+}
---
+++ 2.x/routes/common/pay.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * The file was created by Assimon.
+ *
+ * @author    assimon<ashang@utf8.hk>
+ * @copyright assimon<ashang@utf8.hk>
+ * @link      http://utf8.hk/
+ */
+use Illuminate\Support\Facades\Route;
+
+Route::get('pay-gateway/{handle}/{payway}/{orderSN}', 'PayController@redirectGateway');
+
+// 支付相关
+Route::group(['prefix' => 'pay', 'namespace' => 'Pay', 'middleware' => ['dujiaoka.pay_gate_way']], function () {
+    // 支付宝
+    Route::get('alipay/{payway}/{orderSN}', 'AlipayController@gateway');
+    Route::post('alipay/notify_url', 'AlipayController@notifyUrl');
+    // 微信
+    Route::get('wepay/{payway}/{orderSN}', 'WepayController@gateway');
+    Route::post('wepay/notify_url', 'WepayController@notifyUrl');
+    // 码支付
+    Route::get('mapay/{payway}/{orderSN}', 'MapayController@gateway');
+    Route::post('mapay/notify_url', 'MapayController@notifyUrl');
+    // Paysapi
+    Route::get('paysapi/{payway}/{orderSN}', 'PaysapiController@gateway');
+    Route::post('paysapi/notify_url', 'PaysapiController@notifyUrl');
+    // payjs
+    Route::get('payjs/{payway}/{orderSN}', 'PayjsController@gateway');
+    Route::post('payjs/notify_url', 'PayjsController@notifyUrl');
+    // 易支付
+    Route::get('yipay/{payway}/{orderSN}', 'YipayController@gateway');
+    Route::get('yipay/notify_url', 'YipayController@notifyUrl');
+	
+	// token188
+    Route::get('Token188/{payway}/{orderSN}', 'Token188Controller@gateway');
+    Route::any('Token188/notify_url', 'Token188Controller@notifyUrl');
+	
+    // paypal
+    Route::get('paypal/{payway}/{orderSN}', 'PaypalPayController@gateway');
+    Route::get('paypal/return_url', 'PaypalPayController@returnUrl')->name('paypal-return');
+    Route::any('paypal/notify_url', 'PaypalPayController@notifyUrl');
+    // Mugglepay
+    Route::get('mugglepay/{payway}/{orderSN}', 'MugglepayController@gateway');
+    Route::post('mugglepay/notify_url', 'MugglepayController@notifyUrl');
+    // V免签
+    Route::get('vpay/{payway}/{orderSN}', 'VpayController@gateway');
+    Route::get('vpay/notify_url', 'VpayController@notifyUrl');
+    Route::get('vpay/return_url', 'VpayController@returnUrl')->name('vpay-return');
+    // stripe
+    Route::get('stripe/{payway}/{oid}','StripeController@gateway');
+    Route::get('stripe/return_url','StripeController@returnUrl');
+    Route::get('stripe/check','StripeController@check');
+    Route::get('stripe/charge','StripeController@charge');
+});
